beadcfdfdb50f2ec8781e252d2a9d9d469c81e38
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-05-28 11:03:52 +0530

    cleanups

--- src/main/PincodeField.js
+++ src/main/PincodeField.js
@@ -1,36 +1,50 @@
 import React from 'react';
 
 const PinCodeField = ({ value, onNumberChange, nameOfLabel, isMandatory, hasError, errorMessage, nameOfSecondaryLabel }) => {
-  const onNumberEntered = (e) =>{
-    onNumberChange(e.target.value)
-  }
+  const onNumberEntered = (e) => {
+    const { value } = e.target;
+    if (value.length === 0 || value[0] !== '0') {
+      onNumberChange(value);
+    }
+  };
 
   const inputHandler = (e) => {
     const { value, maxLength } = e.target;
     const key = e.key;
+
     if (
       key === 'Backspace' ||
       key === 'Delete'
     ) {
       return;
     }
-    if (String(value).length >= maxLength) {
+
+    if (value.length >= maxLength) {
       e.preventDefault();
       return;
     }
-  };
 
+    if (value.length === 0 && key === '0') {
+      e.preventDefault();
+    }
+  };
 
   return (
     <div className="form-group py-2">
       <label className='fz-16 lato-regular mb-1'>{nameOfLabel}
-      {nameOfSecondaryLabel !== "Pincode" && <span className='mandatory-class'>{isMandatory? "*": ""}</span>}
+        {nameOfSecondaryLabel !== "Pincode" && <span className='mandatory-class'>{isMandatory ? "*" : ""}</span>}
       </label>
-      <input type="number" className={hasError === true ? "form-control input-error":"form-control"} onChange = {(e) =>onNumberEntered(e)} onKeyDown={inputHandler} maxLength="6"/>
-      {hasError === true && nameOfSecondaryLabel !== "Pincode"  ? <div className='error-message'> {errorMessage} </div>:<div></div>}
+      <input
+        type="number"
+        className={hasError === true ? "form-control input-error" : "form-control"}
+        onChange={onNumberEntered}
+        onKeyDown={inputHandler}
+        maxLength="6"
+      />
+      {hasError === true && nameOfSecondaryLabel !== "Pincode" ? <div className='error-message'> {errorMessage} </div> : <div></div>}
       <label className='fz-12 lato-light mb-1'>{nameOfSecondaryLabel}</label>
-      {nameOfSecondaryLabel === "Pincode" && <span className='mandatory-class'>{isMandatory? "*": ""}</span>}
-      {hasError === true && nameOfSecondaryLabel === "Pincode"  ? <div className='error-message'> {errorMessage} </div>:<div></div>}
+      {nameOfSecondaryLabel === "Pincode" && <span className='mandatory-class'>{isMandatory ? "*" : ""}</span>}
+      {hasError === true && nameOfSecondaryLabel === "Pincode" ? <div className='error-message'> {errorMessage} </div> : <div></div>}
     </div>
   );
 };
